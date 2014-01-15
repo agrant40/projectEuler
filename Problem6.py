49148b28f952a5c94d34e9e94f97ce4d1e65e755
agrant40 <adam.grant40@gmail.com>
2014-01-15 10:36:20 -0600

    updates to Problem6

--- Problem6.py
+++ Problem6.py
@@ -4,5 +4,27 @@
 #(1 + 2 + ... + 10)2 = 552 = 3025
 #Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 #Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-#Answer is
+#Answer is 25164150
+import math
+
+def findSumOfSquares(squares):
+	list = []
+	for square in squares:
+		list.append(math.pow(square,2))
+
+	return (sum(list))
+
+def findSquareOfSum(squares):
+	return (math.pow(sum(squares),2))
+
+number = 100
+squares = []
+count = 1
+
+while len(squares) < number:
+	squares.append(count)
+	count += 1
+
+print (findSquareOfSum(squares) - findSumOfSquares(squares))
+
 
